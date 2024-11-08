bc01ce73d40159ccc1f52d62c2a1da3030b78ebf
sAnjum55 <shadab@gobuncha.com>
2024-11-08 21:20:37 +0530

    db url for deployment

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,13 +1,13 @@
 import pkg from 'pg'
-const { Pool } = pkg
 
+const { Pool } = pkg
+const connectionString = `postgresql://beam_dynamics_test_user:vBCtDrv1oEHo3FmREG9XbDO8eCrXNIEM@dpg-csn1drij1k6c73dq7850-a.singapore-postgres.render.com/beam_dynamics_test`;
 const pool = new Pool({
-    user: 'postgres',
-    host: 'postgres',
-    database: 'roster',
-    password: 'postgres',
-    port: 5432,
-});
+    connectionString: connectionString,
+    ssl: {
+      rejectUnauthorized: false, 
+    },
+  });
 
 export let client = await pool.connect();
 export const startConnection = async () => {
